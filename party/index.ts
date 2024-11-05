59bc9e9e0ae79411132723874c7d78cd893558d3
Drew Alexander <drewalexander986@gmail.com>
2024-11-06 01:28:08 +0800

    I guess that works...

--- party/index.ts
+++ party/index.ts
@@ -1,5 +1,7 @@
 import type * as Party from "partykit/server";
 
+declare const DEVMODE: boolean;
+
 export default class Server implements Party.Server {
   constructor(readonly room: Party.Room) {}
 
@@ -25,6 +27,22 @@ export default class Server implements Party.Server {
       // ...except for the connection it came from
       [sender.id]
     );
+  }
+
+  // Dev Proxy
+  static async onFetch(request: Party.Request) {
+    // when developing locally, we simply proxy all
+    // non-party requests to the vite dev server
+    if (DEVMODE) {
+      const url = new URL(request.url);
+      url.hostname = "localhost";
+      url.port = "5173";
+      return fetch(url.toString(), request as unknown as Request);
+    }
+
+    // You could also add additional api handlers here.
+
+    return new Response("Not found", { status: 404 });
   }
 }
 
